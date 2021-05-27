76f3a28acd89ac3e70849b75928d172695a1dd2e
nOOb3167 <nOOb3167@gmail.com>
2021-05-27 20:09:33 +0200

    stuff

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,16 @@
 #include <memory>
 #include <stdexcept>
 
+#ifdef _DEBUG
+#	define _CM_DEBUG_WAS_ENABLED
+#	undef _DEBUG  // prevents autolinking in python header (pragma comment(lib,"python39_d.lib"))
+#endif
 #include <Python.h>
+#ifdef _CM_DEBUG_WAS_ENABLED
+#	define _DEBUG
+#endif
+
+#include <costrm.h>
 
 template<typename T>
 using sp = ::std::shared_ptr<T>;
@@ -19,6 +28,19 @@ public:
 
 		PyPreConfig_InitIsolatedConfig(&precon);
 		PyConfig_InitIsolatedConfig(&concon);
+
+		// https://docs.python.org/3/c-api/init_config.html#isolated-configuration
+		// https://docs.python.org/3/c-api/init_config.html#path-configuration
+		//   Configuration files are still used with this configuration
+
+		concon.base_exec_prefix = L".";
+		concon.base_executable = L".";
+		concon.base_prefix = L".";
+		concon.exec_prefix = L".";
+		concon.executable = L".";
+		concon.module_search_paths_set = 0;
+		concon.module_search_paths = {};
+		concon.prefix = L".";
 
 		if (PyStatus_Exception(Py_PreInitialize(&precon)))
 			throw std::runtime_error("");
@@ -40,6 +62,7 @@ void sp_pyobject_d(PyObject* a)
 int main(int argc, char** argv)
 {
 	printf("Compiled Against Python Header Version " PY_VERSION "\n");
+	printf("COSTRM_PYPATH " COSTRM_PYPATH "\n");
 
 	CmPyInFin cpif;
 
