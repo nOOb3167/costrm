fecdbfa39ddb109e07580d7d33a702160234042c
nOOb3167 <nOOb3167@gmail.com>
2021-06-02 15:53:50 +0200

    stuff

--- presets.py
+++ presets.py
@@ -1,9 +1,12 @@
 import json
+import logging
 import re
 
 from argparse import ArgumentParser
 from pathlib import Path
 from string import Template
+
+logging.basicConfig(level=logging.INFO, format="%(levelname)s:%(funcName)15s: %(message)s")
 
 """
 NOTE: the double $ as in $${presetName} is an escape,
@@ -51,19 +54,27 @@ def findpyver(p: Path):
     e = "python(.*)\\.dll$"
     w = [re.search(e, x.as_posix()).group(1) for x in p.iterdir() if re.search("python(.*)\\.dll$", x.as_posix())]
     r = max(w, key=lambda x: len(x))  # longest match ex take python39.dll over python3.dll
+    logging.info(f"checked: {p}")
+    logging.info(f"got: {w} {r}")
     return r
 
 def run(boostDir: Path, pyPath: Path):
     cwd: Path = Path.cwd().resolve(strict=True)
     
     sourceDir: Path = cwd / "CMakeLists.txt"
-    assert sourceDir.exists()
 
-    pyVersion: str = findpyver(pyPath) if pyPath else None
+    pyVersion: str = pyPath and findpyver(pyPath) or None
 
     userPresets: Path = cwd / "CMakeUserPresets.json"
 
+    logging.info(f"{pyVersion=}")
+    logging.info(f"{pyPath=}")
+    logging.info(f"{boostDir=}")
+    logging.info(f"{userPresets=} {userPresets.exists()=}")
+
     if not userPresets.exists():
+        logging.info("creating userPresets")
+
         s = Template(TEMPLATE)
         a = s.substitute(sourceDir=sourceDir.as_posix(),
                          boostDir=boostDir and boostDir.as_posix() or "",
@@ -75,16 +86,25 @@ def run(boostDir: Path, pyPath: Path):
             f.write(a)
 
     with userPresets.open(mode="rt") as f:
+        logging.info("validating userPresets")
+
         z = f.read()
         j = json.loads(z)
         
+        logging.info("version")
+
         assert (j["version"] == 2)
         assert ("cmakeMinimumRequired" in j and
                 "major" in j["cmakeMinimumRequired"] and
                 "minor" in j["cmakeMinimumRequired"] and
                 "patch" in j["cmakeMinimumRequired"])
+
+        logging.info("presets")
+
         assert ("configurePresets" in j and
                 "buildPresets" in j)
+
+        logging.info("preset user-configure-base")
 
         a = list(filter(lambda x: x["name"] == "user-configure-base", j["configurePresets"]))[0]
         assert ("cacheVariables" in a and
@@ -94,6 +114,8 @@ def run(boostDir: Path, pyPath: Path):
         assert (Path(a["cacheVariables"]["Boost_DIR"]) / "BoostConfig.cmake").exists()
         assert (Path(a["cacheVariables"]["COSTRM_PYPATH"]) / "python.exe").exists()
         assert (Path(a["cacheVariables"]["COSTRM_PYPATH"]) / f"python{a['cacheVariables']['COSTRM_PYVERS']}.dll").exists()
+
+        logging.info("completed")
 
 if __name__ == '__main__':
     import sys
